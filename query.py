9af4106886d288ae9eeabaafe2835a863432939a
mjam03 <mjam03@users.noreply.github.com>
2020-10-06 10:39:36 +0100

    Add initial db query functions
    Initial commit with sqlite query functions and gitignore

---
+++ query.py
@@ -0,0 +1,59 @@
+import datetime as dt
+import numpy as np
+import os
+import pandas as pd
+import sqlite3
+
+
+# data path for the sqlite database
+DB_PATH = '/Users/jamisonm/dev/epl/data/match_results.sqlite'
+
+
+def query_db(query):
+    '''
+    Generic function to hit the database and return results
+    Query: str sql query statement
+    '''
+    try:
+        conn = sqlite3.connect(DB_PATH)
+    except:
+        return "Unable to establish connection to {}".format(DB_PATH)
+    
+    try:
+        print('Running query: {}'.format(query))
+        res = pd.read_sql(query, conn)
+    except:
+        return "Unable to run query: {}".format(query)
+    return res
+
+
+def query_creator(table, cols=None, wc=None):
+    '''
+    Fn to format a query string using args
+    table: str for the table in the db to query from
+    cols: list of the columns we wish to query ['HomeTeam', 'AwayTeam']
+    wc: dict , wc=
+    '''
+    if wc:
+        conds = []
+        for col,cond in wc.items():
+            if isinstance(cond[1], str):
+                conds.append("{} {} '{}'".format(col, cond[0], cond[1]))
+            else:
+                conds.append("{} {} {}".format(col, cond[0], cond[1]))
+        wc = ' AND '.join(conds)
+    else:
+        wc = ''
+    
+    if cols:
+        col_query = ', '.join(cols)
+    else:
+        col_query = '*'
+    
+    query = 'SELECT {} FROM {} WHERE {}'.format(col_query, table, wc)
+    return query
+
+
+if __name__ == "__main__":
+    None
+ 
